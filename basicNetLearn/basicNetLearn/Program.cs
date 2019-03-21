881642166ed227f7d94c9e63b1ddd55b69e9b312
rynxiao <yuzhongzi91@sina.com>
2019-03-21 23:32:58 +0800

    [feat]ryn: add chapter 1 - 7 demos

--- basicNetLearn/basicNetLearn/Program.cs
+++ basicNetLearn/basicNetLearn/Program.cs
@@ -1,15 +1,72 @@
 ﻿using System;
+using System.Collections;
 
 namespace basicNetLearn
 {
     class MainClass
     {
+        class C
+        {
+            public int Value = 0;
+        }
+
+        enum MyDate
+        {
+            Sun = 0,
+            Mon = 1,
+            Tue = 2,
+            Wed = 3,
+            Thi = 4,
+            Fri = 5,
+            Sat = 6
+        }
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
+            int ls = 927;
+            byte shj = 255;
+            Console.WriteLine("ls={0}", ls);
+            Console.WriteLine("shj={0}", shj);
+
+            int v1 = 0;
+            int v2 = v1;
+            v2 = 927;
+            C r1 = new C();
+            C r2 = r1;
+            r2.Value = 112;
+            Console.WriteLine("Values: {0},{1}", v1, v2);
+            Console.WriteLine("Refs: {0},{1}", r1.Value, r2.Value);
+
+            int k = (int) DateTime.Now.DayOfWeek;
+            switch (k)
+            {
+                case (int) MyDate.Sun:
+                    Console.WriteLine("今天星期天");
+                    break;
+                case (int) MyDate.Mon:
+                    Console.WriteLine("今天星期一");
+                    break;
+                case (int) MyDate.Tue:
+                    Console.WriteLine("今天星期二");
+                    break;
+                case (int) MyDate.Wed:
+                    Console.WriteLine("今天星期三");
+                    break;
+                case (int) MyDate.Thi:
+                    Console.WriteLine("今天星期四");
+                    break;
+                case (int) MyDate.Fri:
+                    Console.WriteLine("今天星期五");
+                    break;
+                case (int) MyDate.Sat:
+                    Console.WriteLine("今天星期六");
+                    break;
+            }
+
             double x = 1993545.0112;
-            int y = (int)x;
+            int y = (int) x;
             Console.WriteLine(y);
 
             int z = Convert.ToInt32(y);
@@ -31,8 +88,8 @@ namespace basicNetLearn
             //String yesOrNo = isLeapYear ? "是" : "不是";
             //Console.WriteLine("{0}年{1}闰年", year, yesOrNo);
 
-            String[] ls = new string[2] { "1", "2" };
-            Console.WriteLine(ls[0]);
+            String[] ls2 = new string[2] {"1", "2"};
+            Console.WriteLine(ls2[0]);
 
             Type myType = typeof(int);
             Console.WriteLine("类型：{0}", myType);
@@ -41,6 +98,26 @@ namespace basicNetLearn
             String str2 = "testHelloWorld";
             Console.WriteLine(String.Compare(str1, str2));
             Console.WriteLine(str1.CompareTo(str2));
+
+            DateTime dt = DateTime.Now;
+            String strB = String.Format("{0:D}", dt);
+            Console.WriteLine(strB);
+            
+            // PadLeft, PadRight
+            String strPad = "*^___^*";
+            String str12 = strPad.PadLeft(7, '(').PadRight(8, ')');
+            Console.WriteLine(str12);
+            
+            // HashTable
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("id", "001");
+            hashtable.Add("sex", "男");
+            hashtable.Add("name", "张三");
+            foreach (DictionaryEntry dicEntry in hashtable)
+            {
+                Console.WriteLine("\t" + dicEntry.Key + "\t" + dicEntry.Value);
+            }
+            Console.WriteLine(hashtable.ContainsKey("id"));
         }
     }
 }
